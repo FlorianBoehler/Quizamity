e56a650a74d883fa3fd27fa5c0688fb5513fb475
Selina Collini <selina.collini@iu-study.org>
2025-04-16 22:24:00 +0200

    installed jest and necessary dependencies tried some simple tets - everything seems to work

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // Für moderne JavaScript-Features
+    "@babel/preset-react", // Für JSX-Syntax in React
+  ],
+};
---
+++ quizamity_frontend/src/styles/tests/Test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = ({ text }) => {
+  return <div>{text}</div>;
+};
+
+export default Test;
