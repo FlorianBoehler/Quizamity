9171192c98a9e18938555ab22b0f18a8b26ac5d8
florian <florian.boehler@gmail.com>
2025-07-03 15:30:59 +0200

    cors

--- src/main/java/com/quizamity/filter/CORSFilter.java
+++ src/main/java/com/quizamity/filter/CORSFilter.java
@@ -8,17 +8,27 @@ import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Provider
 @PreMatching
 public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
+    private static final Set<String> ALLOWED_ORIGINS = new HashSet<>();
+
+    static {
+        ALLOWED_ORIGINS.add("http://localhost:8080");
+        ALLOWED_ORIGINS.add("http://13.61.4.44:8080");
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        // Handle preflight OPTIONS requests
-        if ("OPTIONS".equals(requestContext.getMethod())) {
+        String origin = requestContext.getHeaderString("Origin");
+
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod()) && origin != null && ALLOWED_ORIGINS.contains(origin)) {
             requestContext.abortWith(Response.ok()
-                    .header("Access-Control-Allow-Origin", "http://localhost:8080")
+                    .header("Access-Control-Allow-Origin", origin)
                     .header("Access-Control-Allow-Credentials", "true")
                     .header("Access-Control-Allow-Headers",
                             "origin, content-type, accept, authorization, x-requested-with")
@@ -32,14 +42,16 @@ public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilt
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
 
-        // Add CORS headers to all responses (including error responses)
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization, x-requested-with");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+        if (origin != null && ALLOWED_ORIGINS.contains(origin)) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            responseContext.getHeaders().add("Access-Control-Allow-Headers",
+                    "origin, content-type, accept, authorization, x-requested-with");
+            responseContext.getHeaders().add("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+        }
     }
 }
